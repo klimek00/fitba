cbf88f3ae3815605ff74090fe4fcb20eac9aab6b
Mateusz Klimczak <torxnados@gmail.com>
2023-06-23 01:49:49 +0200

    Update body.js

--- src/routes/body.js
+++ src/routes/body.js
@@ -5,7 +5,9 @@ export default function Body() {
     <>
       <div className="h-screen p-6">
         in build. always will be.<br/>
-        you can always <Link to="soogulator"><span className="text-blue-600 visited:text-purple-600">Soogulate</span></Link> your food
+        you can always <Link to="soogulator"><span className="text-blue-600 visited:text-purple-600">Soogulate</span></Link> your food<br/>
+        or learn <Link to='about'><span className="text-blue-600 visited:text-purple-600">something about me</span></Link><br/>
+        just make sure you won't <Link to='about'><span className="text-blue-600 visited:text-purple-600">get lost</span></Link>
 
       </div>
     </>
